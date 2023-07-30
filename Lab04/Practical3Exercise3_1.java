673715d1b28e1d3eedb016658a4540f16f17d616
Sandith Kariyawasam <136833383+sandith2001@users.noreply.github.com>
2023-07-30 10:13:11 +0530

    Practical3Exercise3_1.java

---
+++ Lab04/Practical3Exercise3_1.java
@@ -0,0 +1,11 @@
+package com.mycompany.practical3_exercise3_1;
+
+public class Practical3_Exercise3_1 {
+
+    public static void main(String[] args) {
+        Employee e1 = new Employee("Binod",22,55000.00f);
+        System.out.println("Emplyee name: "+ e1.getname());
+        System.out.println("Employee age: "+ e1.getage());
+        System.out.println("Employee salary: "+ e1.getsalary());
+    }
+}
